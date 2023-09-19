79099177d8fae1ce6386d2888be73f40e2bf8fad
Christian Gröling <contact@christiangroeling.de>
2023-09-19 13:36:32 +0200

    Add unit tests for cloning Content

--- emerald/src/types/content.rs
+++ emerald/src/types/content.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 
 /// This struct holds a reference counted string
 /// to hold content from markdown files.
@@ -14,5 +14,26 @@ impl From<&str> for Content {
 impl From<String> for Content {
     fn from(value: String) -> Self {
         Self(Rc::new(value))
+    }
+}
+
+impl Clone for Content {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use super::Content;
+    #[test]
+    fn test_clone() {
+        let s1: Content = "Hello world".into();
+        let s2 = s1.clone();
+
+        assert_eq!(Rc::strong_count(&s1.0), 2);
+        assert_eq!(*s2.0, "Hello world"); // ensure that s2 is not deconstructed until this point
     }
 }
