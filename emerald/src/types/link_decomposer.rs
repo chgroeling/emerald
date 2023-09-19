9f7ebbc9829685c0ca56368b1f68828dd39c928a
Christian Gröling <contact@christiangroeling.de>
2023-09-19 20:31:41 +0200

    Refactoring

--- emerald/src/types/link_decomposer.rs
+++ emerald/src/types/link_decomposer.rs
@@ -1,6 +1,8 @@
 use super::res_and_err::{EmeraldError, Result};
 use std::fmt::Display;
 
+use EmeraldError::*;
+
 #[derive(Debug)]
 pub struct DecomposedLink {
     pub path: Option<String>,
@@ -71,81 +73,82 @@ impl From<&'static str> for DecomposedLink {
 
 pub struct LinkDecomposer {}
 
-fn extract_part(s: &str) -> (&str, Option<&str>) {
-    let end_idx = s.find(|c| c == '|' || c == '#' || c == '^');
-    if end_idx.is_none() {
-        return (s, None);
-    }
-
-    let end_idx = end_idx.map_or_else(|| s.len(), |x| x);
-
-    let front = &s[..end_idx];
-    let back = &s[end_idx..];
-
-    (front, Some(back))
-}
-fn extract_wiki_link(s: &str) -> Option<DecomposedLink> {
-    let start = s.find("[[")?;
-    let end = s.find("]]")?;
-
-    // check if "[[" is at the start of the string
-    if start != 0 {
-        return None;
-    }
-
-    // check if "]]" is at the end of the string
-    if end != s.len() - 2 {
-        return None;
-    }
-
-    // sanity check
-    if start >= end {
-        return None;
-    }
-
-    // the link text is inbetween the braces
-    let link_text = &s[(start + 2)..end];
-
-    // split string in half at the position of the first occurence of #^|
-    let (mut front, mut back) = extract_part(link_text);
-
-    // Get the full link and path if exists
-    let full_link = front;
-    let link_parts: Vec<&str> = full_link.split('/').collect();
-    let link = link_parts.last().unwrap().to_string();
-
-    let path: Option<String> = if link_parts.len() > 1 {
-        Some(full_link[0..(full_link.len() - link_parts.last().unwrap().len() - 1)].to_owned())
-    } else {
-        None
-    };
-
-    let mut label: Option<String> = None;
-    let mut section: Option<String> = None;
-    let mut anchor: Option<String> = None;
-
-    // Extract the rest
-    while let Some(rest) = back {
-        let first_char = &rest[0..1];
-        (front, back) = extract_part(&rest[1..]);
-
-        match first_char {
-            "|" => label = Some(front.to_owned()),
-            "#" => section = Some(front.to_owned()),
-            "^" => anchor = Some(front.to_owned()),
-            _ => (),
-        }
-    }
-    return Some(DecomposedLink::new(link, path, label, section, anchor));
-}
-
 impl LinkDecomposer {
     pub fn new() -> LinkDecomposer {
         LinkDecomposer {}
     }
 
-    pub fn decompose(&self, link: &str) -> Result<DecomposedLink> {
-        Ok(extract_wiki_link(link).ok_or(EmeraldError::NotAWikiLink)?)
+    #[inline]
+    fn extract_part<'a>(&self, s: &'a str) -> (&'a str, Option<&'a str>) {
+        let end_idx = s.find(|c| c == '|' || c == '#' || c == '^');
+        if end_idx.is_none() {
+            return (s, None);
+        }
+
+        let end_idx = end_idx.map_or_else(|| s.len(), |x| x);
+
+        let front = &s[..end_idx];
+        let back = &s[end_idx..];
+
+        (front, Some(back))
+    }
+
+    pub fn decompose(&self, s: &str) -> Result<DecomposedLink> {
+        let start = s.find("[[").ok_or(NotAWikiLink)?;
+        let end = s.find("]]").ok_or(NotAWikiLink)?;
+
+        // check if "[[" is at the start of the string
+        if start != 0 {
+            return Err(NotAWikiLink);
+        }
+
+        // check if "]]" is at the end of the string
+        if end != s.len() - 2 {
+            return Err(NotAWikiLink);
+        }
+
+        // sanity check
+        if start >= end {
+            return Err(NotAWikiLink);
+        }
+
+        // the link text is inbetween the braces
+        let link_text = &s[(start + 2)..end];
+
+        // split string in half at the position of the first occurence of #^|
+        let (mut front, mut back) = self.extract_part(link_text);
+
+        // Get the full link and path if exists
+        let full_link = front;
+        let link_parts: Vec<&str> = full_link.split('/').collect();
+        let link = link_parts.last().unwrap().to_string();
+
+        let path: Option<String> = if link_parts.len() > 1 {
+            Some(full_link[0..(full_link.len() - link_parts.last().unwrap().len() - 1)].to_owned())
+        } else {
+            None
+        };
+
+        let mut label: Option<String> = None;
+        let mut section: Option<String> = None;
+        let mut anchor: Option<String> = None;
+
+        // Extract the rest
+        while let Some(rest) = back {
+            let first_char = &rest[0..1];
+
+            // do look for the next occurance of #^| if any
+            (front, back) = self.extract_part(&rest[1..]);
+
+            match first_char {
+                "|" => label = Some(front.to_owned()),
+                "#" => section = Some(front.to_owned()),
+                "^" => anchor = Some(front.to_owned()),
+                _ => (),
+            }
+        }
+
+        return Ok(DecomposedLink::new(link, path, label, section, anchor));
     }
 }
 
