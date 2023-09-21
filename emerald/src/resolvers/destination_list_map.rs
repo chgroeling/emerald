356e8056b46479b71eea927607c1035ea506a4a1
Christian Gröling <contact@christiangroeling.de>
2023-09-21 09:16:07 +0200

    Prepared impl of DestinationListResolver

--- emerald/src/resolvers/destination_list_map.rs
+++ emerald/src/resolvers/destination_list_map.rs
@@ -2,7 +2,7 @@ use std::collections::{hash_map::Entry, HashMap};
 
 use crate::{indexes::AllNoteLinksIterSource, types::ResourceId};
 
-use super::destination_list_resolver::ListOfLinksWithDestination;
+use super::destination_list_resolver::{DestinationListResolver, ListOfLinksWithDestination};
 
 type OriginToDestinationListMap = HashMap<ResourceId, ListOfLinksWithDestination>;
 struct DestinationListMap {
@@ -15,7 +15,7 @@ impl DestinationListMap {
         for link_to_dest in all_note_links_iter_source.all_iter() {
             let origin = link_to_dest.origin;
             let link_and_dest = link_to_dest.link_and_destination;
-            // this is an interesting way to mutate an element in a HashMap
+
             match origin_to_destination.entry(origin) {
                 Entry::Occupied(mut e) => {
                     e.get_mut().push(link_and_dest);
@@ -28,5 +28,11 @@ impl DestinationListMap {
         Self {
             origin_to_destination,
         }
+    }
+}
+
+impl DestinationListResolver for DestinationListMap {
+    fn resolve(&self, resource_id: ResourceId) -> std::vec::IntoIter<ListOfLinksWithDestination> {
+        todo!()
     }
 }
