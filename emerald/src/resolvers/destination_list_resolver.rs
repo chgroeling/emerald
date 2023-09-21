8adccad1e7bb6b63c868cfe30c2951d550b89949
Christian Gröling <contact@christiangroeling.de>
2023-09-21 07:34:11 +0200

    Add more context.

--- emerald/src/resolvers/destination_list_resolver.rs
+++ emerald/src/resolvers/destination_list_resolver.rs
@@ -2,7 +2,7 @@ use crate::types::{LinkAndResourceId, ResourceId};
 
 pub type LinkWithDestinationList = Vec<LinkAndResourceId>;
 
-/// This trait is used to resolve all links which are orignating from a note
+/// This trait is used to resolve all links with their desitnation which are orignating from a note
 /// given by `resource_id`
 pub trait DestinationListResolver {
     fn resolve(&self, resource_id: ResourceId) -> LinkWithDestinationList;
