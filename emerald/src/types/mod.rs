669ca042065380744225ce212db6ad0f8ff2036b
Christian Gröling <contact@christiangroeling.de>
2023-09-21 09:53:16 +0200

    Renamed LinkAndResource to LinkAndDestination

--- emerald/src/types/mod.rs
+++ emerald/src/types/mod.rs
@@ -1,7 +1,7 @@
 mod content;
 mod endpoint;
 pub mod link;
-mod link_and_resource_id;
+mod link_and_destination;
 mod link_components;
 pub mod link_decomposer;
 mod origin_to_destination;
@@ -14,6 +14,6 @@ pub use self::res_and_err::EmeraldError;
 pub type Link = self::link::Link;
 pub type ResourceId = self::resource_id::ResourceId;
 pub type Result<T> = self::res_and_err::Result<T>;
-pub type LinkAndResourceId = self::link_and_resource_id::LinkAndResourceId;
+pub type LinkAndResourceId = self::link_and_destination::LinkAndDestination;
 pub use self::endpoint::EndPoint;
 pub use self::origin_to_destination::OriginToDestination;
