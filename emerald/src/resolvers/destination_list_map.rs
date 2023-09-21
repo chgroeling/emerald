5d3abe1c233488f99e6906c54cd95f7bf6775a3e
Christian Gröling <contact@christiangroeling.de>
2023-09-21 07:48:32 +0200

    Finished mapping algorithm

--- emerald/src/resolvers/destination_list_map.rs
+++ emerald/src/resolvers/destination_list_map.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{hash_map::Entry, HashMap};
 
 use crate::{indexes::AllNoteLinksIterSource, types::ResourceId};
 
@@ -11,10 +11,19 @@ struct DestinationListMap {
 
 impl DestinationListMap {
     pub fn new(all_note_links_iter_source: &impl AllNoteLinksIterSource) -> Self {
-        let origin_to_destination = OriginToDestinationListMap::new();
+        let mut origin_to_destination = OriginToDestinationListMap::new();
         for link_to_dest in all_note_links_iter_source.all_iter() {
-
-            //
+            let origin = link_to_dest.origin;
+            let link_and_dest = link_to_dest.link_and_destination;
+            // this is an interesting way to mutate an element in a HashMap
+            match origin_to_destination.entry(origin) {
+                Entry::Occupied(mut e) => {
+                    e.get_mut().push(link_and_dest);
+                }
+                Entry::Vacant(e) => {
+                    e.insert(vec![link_and_dest]);
+                }
+            }
         }
         Self {
             origin_to_destination,
