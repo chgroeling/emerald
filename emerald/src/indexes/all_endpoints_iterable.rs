1d547ff69873965f4e492d5aaba521f84b5bbf4e
Christian Gröling <contact@christiangroeling.de>
2023-09-25 07:27:19 +0300

    Add Mock to all_endpoints_iterable.rs

--- emerald/src/indexes/all_endpoints_iterable.rs
+++ emerald/src/indexes/all_endpoints_iterable.rs
@@ -1,5 +1,9 @@
 use crate::types::EndPoint;
 
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+
+#[cfg_attr(test, automock(type Iter=std::vec::IntoIter<EndPoint>;))]
 pub trait AllEndpointsIterable {
     type Iter: Iterator<Item = EndPoint>;
     fn all_iter(&self) -> Self::Iter;
