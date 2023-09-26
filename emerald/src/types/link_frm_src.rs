96201f8cc15931989f0852b0b336c5b3726e8c6e
Christian Gröling <contact@christiangroeling.de>
2023-09-26 07:33:25 +0300

    Aligned names

--- emerald/src/types/link_frm_src.rs
+++ emerald/src/types/link_frm_src.rs
@@ -2,12 +2,12 @@ use super::{Link, ResourceId};
 
 #[derive(Debug, Clone, PartialEq)]
 // Structs holds a link and the resource id from which the links stems.
-pub struct LinkFromSource {
+pub struct LinkFrmSrc {
     pub link: Link,
     pub source: ResourceId,
 }
 
-impl LinkFromSource {
+impl LinkFrmSrc {
     pub fn new(link: Link, source: ResourceId) -> Self {
         Self { source, link }
     }
