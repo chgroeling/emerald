5db4f028a61aa60b0cf17a8997d9cf6213c87a54
Christian Gröling <contact@christiangroeling.de>
2023-09-27 08:18:21 +0300

    Removed superflous code

--- emerald/src/types/resource_id.rs
+++ emerald/src/types/resource_id.rs
@@ -1,5 +1,3 @@
-use super::link::Link;
-
 #[derive(Debug, Clone, PartialEq, Hash)]
 
 /// A ResourceId points to a unique Resource
@@ -7,13 +5,6 @@ use super::link::Link;
 /// Currently a ResourceId is nothing else than a string containing a path
 /// to the filesystem
 pub struct ResourceId(pub String);
-
-impl ResourceId {
-    #[allow(dead_code)]
-    pub fn downgrad(self) -> Link {
-        Link(self.0)
-    }
-}
 
 // Allows to use a string as a ResourceId
 impl From<&str> for ResourceId {
