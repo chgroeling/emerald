553cdd8237ab4048e3118cedde2bd7badbacf798
Christian Gröling <contact@christiangroeling.de>
2023-10-08 06:31:50 +0200

    Refactoring

--- emerald/src/resources/resource_id_resolver.rs
+++ emerald/src/resources/resource_id_resolver.rs
@@ -2,9 +2,25 @@ use crate::types::{EndPoint, ResourceId};
 use crate::Result;
 
 #[cfg(test)]
-use mockall::{automock, predicate::*};
+use mockall::{mock, predicate::*};
 
-#[cfg_attr(test, automock)]
 pub trait ResourceIdResolver {
     fn resolve(&self, endpoint: &EndPoint) -> Result<ResourceId>;
+}
+
+#[cfg(test)]
+mock! {
+
+    pub ResourceIdResolver{}
+
+    impl ResourceIdResolver for ResourceIdResolver {
+        fn resolve(&self, endpoint: &EndPoint) -> Result<ResourceId>;
+    }
+
+    impl Clone for ResourceIdResolver {
+        fn clone(&self) -> Self {
+            self.clone()
+        }
+    }
+
 }
