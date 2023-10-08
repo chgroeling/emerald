afbbdac61873cd4e80888c616ddc0174d06c4ef7
Christian Gröling <contact@christiangroeling.de>
2023-10-08 11:38:02 +0200

    src 2 tgt index is now clonable

--- emerald/src/indexes/src_2_tgt_index.rs
+++ emerald/src/indexes/src_2_tgt_index.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
@@ -9,10 +11,11 @@ use crate::{
 
 use super::{src_2_tgt_iter_src::Src2TgtIterSrc, ResourceIdsIterSrc};
 
+#[derive(Clone)]
 pub struct Src2TargetIndex {
     valid_backlink_cnt: usize,
     invalid_backlink_cnt: usize,
-    src_2_tgt_list: Vec<LinkSrc2Tgt>,
+    src_2_tgt_list: Rc<Vec<LinkSrc2Tgt>>,
 }
 
 impl Src2TargetIndex {
@@ -54,7 +57,7 @@ impl Src2TargetIndex {
         Self {
             valid_backlink_cnt,
             invalid_backlink_cnt,
-            src_2_tgt_list,
+            src_2_tgt_list: Rc::new(src_2_tgt_list),
         }
     }
 
@@ -70,6 +73,6 @@ impl Src2TargetIndex {
 impl Src2TgtIterSrc for Src2TargetIndex {
     type Iter = std::vec::IntoIter<LinkSrc2Tgt>;
     fn iter(&self) -> Self::Iter {
-        self.src_2_tgt_list.clone().into_iter()
+        (*self.src_2_tgt_list).clone().into_iter()
     }
 }
