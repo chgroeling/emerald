1cd6380632454a1cebbf311e659fdf9cd1cabf22
Christian Gröling <contact@christiangroeling.de>
2023-10-09 11:02:11 +0200

    PRovided abstract form of call

--- emerald/src/content_analyzers/md_link_analyzer.rs
+++ emerald/src/content_analyzers/md_link_analyzer.rs
@@ -1,5 +1,5 @@
 use crate::{
-    maps::ResourceIdRetriever,
+    maps::{resource_id_link_map::ResourceIdLinkMap, ResourceIdRetriever},
     types::{Content, Link2Tgt},
 };
 
@@ -15,6 +15,20 @@ pub fn extract_md_links(
     let content_type_iter = extract_content_types(content);
     let link_iter = extract_links(content_type_iter);
     extract_resource(link_iter, resource_id_retriever)
+}
+
+type AbstractLinkExtractor<TResRetriever, C> = fn(Content, TResRetriever) -> C;
+
+pub fn arg_extract<TResRetriever, C>(
+    md_link_extractor: AbstractLinkExtractor<TResRetriever, C>,
+    content: Content,
+    resource_id_retriever: TResRetriever,
+) -> impl Iterator<Item = Link2Tgt>
+where
+    TResRetriever: ResourceIdRetriever,
+    C: Iterator<Item = Link2Tgt>,
+{
+    md_link_extractor(content, resource_id_retriever)
 }
 
 #[derive(Clone)]
@@ -43,8 +57,15 @@ where
     type Iter = std::vec::IntoIter<Link2Tgt>;
 
     fn iter(&self, content: Content) -> Self::Iter {
-        let ret: Vec<Link2Tgt> =
-            extract_md_links(content, self.resource_id_retriever.clone()).collect();
+        // let ret: Vec<Link2Tgt> =
+        //   extract_md_links(content.clone(), self.resource_id_retriever.clone()).collect();
+
+        let ret: Vec<Link2Tgt> = arg_extract(
+            extract_md_links,
+            content,
+            self.resource_id_retriever.clone(),
+        )
+        .collect();
         ret.into_iter()
     }
 }
