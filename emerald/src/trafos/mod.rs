660b474c9b112b0db35fbcef2bf4c7cf25cc5839
Christian Gröling <contact@christiangroeling.de>
2023-10-12 11:15:47 +0200

    Refactoring

--- emerald/src/trafos/mod.rs
+++ emerald/src/trafos/mod.rs
@@ -1,17 +1,17 @@
 use crate::types::LinkSrc2Tgt;
 
-mod extract_links_from_vault;
 mod trafo_to_content;
 mod trafo_to_content_types;
 mod trafo_to_filetype_and_resource_id;
 mod trafo_to_link_2_tgt;
 mod trafo_to_link_src_2_tgt;
 mod trafo_to_links;
+mod trafo_to_linksrc2tgt;
 mod trafo_to_resource_id;
 
-pub use extract_links_from_vault::trafo_from_content_to_linksrc2tgt;
 pub use trafo_to_content::trafo_resource_ids_to_content;
 pub use trafo_to_filetype_and_resource_id::trafo_to_filetype_and_resource_id;
+pub use trafo_to_linksrc2tgt::trafo_from_content_to_linksrc2tgt;
 pub use trafo_to_resource_id::filter_markdown_types;
 pub use trafo_to_resource_id::trafo_ep_to_rid;
 
