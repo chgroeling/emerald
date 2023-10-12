f0a4360f9241cd667266ed63af8f87bd6b51ff37
Christian Gröling <contact@christiangroeling.de>
2023-10-12 09:31:58 +0200

    Refactoring

--- emerald/src/trafos/mod.rs
+++ emerald/src/trafos/mod.rs
@@ -4,10 +4,10 @@ mod convert_to_link_2_tgt;
 mod convert_to_link_src_2_tgt;
 mod extract_content_types;
 mod extract_links;
-mod extract_links_from_content;
+mod extract_links_from_vault;
 mod trafo_to_filetype_and_resource_id;
 
-pub use extract_links_from_content::extract_links_from_vault;
+pub use extract_links_from_vault::extract_links_from_vault;
 pub use trafo_to_filetype_and_resource_id::trafo_to_filetype_and_resource_id;
 
 pub type LinkSrc2TgtIterBoxed<'a> = Box<dyn Iterator<Item = LinkSrc2Tgt> + 'a>;
