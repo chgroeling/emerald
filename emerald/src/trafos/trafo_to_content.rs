4ea05dcf7fcaf5e49213e94defe70e4f82280686
Christian Gröling <contact@christiangroeling.de>
2023-10-13 09:22:08 +0200

    Refactoring

--- emerald/src/trafos/trafo_to_content.rs
+++ emerald/src/trafos/trafo_to_content.rs
@@ -3,10 +3,10 @@ use crate::types::{Content, ResourceId};
 use crate::Result;
 
 pub fn trafo_resource_ids_to_content<'a>(
-    iter: impl Iterator<Item = &'a ResourceId> + 'a,
+    md_res_id_iter: impl Iterator<Item = &'a ResourceId>,
     content_retriever: &'a impl MdContentRetriever,
-) -> impl Iterator<Item = (ResourceId, Result<&Content>)> + 'a {
+) -> impl Iterator<Item = (ResourceId, Result<&'a Content>)> {
     // load content.
     // iterator yields (ResourceId, Result<Content>)
-    iter.map(move |f| (f.clone(), content_retriever.retrieve(&f)))
+    md_res_id_iter.map(move |f| (f.clone(), content_retriever.retrieve(&f)))
 }
