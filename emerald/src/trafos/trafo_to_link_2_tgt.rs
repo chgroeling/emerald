0023bbc08152a996b24fbd56af5782f871e2bb25
Christian Gröling <contact@christiangroeling.de>
2023-10-15 16:23:10 +0200

    Removed iterator

--- emerald/src/trafos/trafo_to_link_2_tgt.rs
+++ emerald/src/trafos/trafo_to_link_2_tgt.rs
@@ -6,38 +6,15 @@ use crate::{
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
-pub struct ConvertToLink2TgtIterator<'a, I, U>
-where
-    I: Iterator<Item = Link>,
-    U: ResourceIdRetriever,
-{
-    input_iter: I,
-    resource_id_retriever: &'a U,
-}
-
-impl<'a, I, U> Iterator for ConvertToLink2TgtIterator<'a, I, U>
-where
-    I: Iterator<Item = Link>,
-    U: ResourceIdRetriever,
-{
-    type Item = Link2Tgt;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let link_candidate = self.input_iter.next()?;
-        if let Ok(resource_id) = self.resource_id_retriever.retrieve(&link_candidate) {
-            Some(Link2Tgt::new(link_candidate, Some(resource_id)))
-        } else {
-            Some(Link2Tgt::new(link_candidate, None))
-        }
-    }
-}
-
 pub fn trafo_from_links_to_link_2_tgt<'a>(
     iter: impl Iterator<Item = Link> + 'a,
     resource_id_retriever: &'a impl ResourceIdRetriever,
 ) -> impl Iterator<Item = Link2Tgt> + 'a {
-    ConvertToLink2TgtIterator {
-        input_iter: iter,
-        resource_id_retriever,
-    }
+    iter.map(|f| {
+        if let Ok(resource_id) = resource_id_retriever.retrieve(&f) {
+            Link2Tgt::new(f, Some(resource_id))
+        } else {
+            Link2Tgt::new(f, None)
+        }
+    })
 }
