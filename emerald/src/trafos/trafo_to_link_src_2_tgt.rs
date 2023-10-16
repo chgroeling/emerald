00a6d6017eaee13e42e0b48e66b1fd397ada1c41
Christian Gröling <contact@christiangroeling.de>
2023-10-16 09:34:17 +0200

    Refactoring

--- emerald/src/trafos/trafo_to_link_src_2_tgt.rs
+++ emerald/src/trafos/trafo_to_link_src_2_tgt.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use super::trafo_to_content_types::trafo_from_content_to_content_type;
 use super::trafo_to_link_2_tgt::trafo_from_links_to_link_2_tgt;
 use super::trafo_to_links::trafo_from_content_type_to_links;
@@ -83,7 +85,12 @@ where
     I: Fn(&'a str) -> Iter,
     Iter: Iterator<Item = ContentType<'a>> + 'a,
 {
-    let unwrap_iter = iter.map(|f| (f.0, f.1.unwrap()));
+    let unwrap_iter = iter.filter_map(|f| {
+        if let Ok(content) = f.1 {
+            return Some((f.0, content));
+        }
+        None
+    });
     // iterator yield (a, b)
     // a: the resource id of the source which was loaded
     // b: a vector containing the links which were found wrapped in a Result
