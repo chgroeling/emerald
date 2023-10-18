65a6faa06f847e120163677a409731c5977f8a88
Christian Gröling <contact@christiangroeling.de>
2023-10-18 13:42:45 +0200

    Introduced trait

--- emerald/src/md_analyzer/mod.rs
+++ emerald/src/md_analyzer/mod.rs
@@ -7,3 +7,17 @@ mod markdown_iterator_state;
 pub fn analyze_markdown(md_str: &str) -> impl Iterator<Item = ContentType> {
     MarkdownAnalyzerIter::new(md_str)
 }
+
+struct AnalyzeMarkdownImpl;
+trait AnalyzeMarkdown<'a> {
+    type Iterator: Iterator<Item = ContentType<'a>> + 'a;
+    fn analyze(md_str: &'a str) -> Self::Iterator;
+}
+
+impl<'a> AnalyzeMarkdown<'a> for AnalyzeMarkdownImpl {
+    type Iterator = MarkdownAnalyzerIter<'a>;
+
+    fn analyze(md_str: &'a str) -> Self::Iterator {
+        MarkdownAnalyzerIter::new(md_str)
+    }
+}
