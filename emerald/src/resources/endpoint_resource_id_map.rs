84827242c3454d5bc43907303d954d0b15be6175
Christian Gröling <contact@christiangroeling.de>
2023-10-20 09:12:21 +0200

    Refactoring

--- emerald/src/resources/endpoint_resource_id_map.rs
+++ emerald/src/resources/endpoint_resource_id_map.rs
@@ -1,4 +1,4 @@
-use crate::{resources::resource_id_retriever::ResourceIdRetriever, EmeraldError, Result};
+use crate::{EmeraldError, Result};
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 use std::{collections::HashMap, rc::Rc};
@@ -34,58 +34,4 @@ impl EndpointRetriever for EndpointResourceIdMap {
             .get(resource_id)
             .map_or(Err(EndPointNotFound), |f| Ok(f.clone()))
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::EndPoint;
-    use super::EndpointResourceIdMap;
-    use crate::resources::endpoint_retriever::EndpointRetriever;
-    use crate::resources::resource_id_retriever::MockResourceIdResolver;
-    use crate::types::ResourceId;
-    use std::path::PathBuf;
-    /*
-    fn create_dut(test_ep_list: Vec<EndPoint>) -> EndpointResourceIdMap {
-        let mut mock_res_id_res = MockResourceIdResolver::new();
-        mock_res_id_res
-            .expect_retrieve()
-            .withf(|f| matches!(f, EndPoint::FileUnknown(path) if path==&PathBuf::from("testpath")))
-            .returning(|_f| Ok(ResourceId("[[testpath]]".to_string())));
-
-        let dut = EndpointResourceIdMap::new(test_ep_list.iter(), &mock_res_id_res);
-        dut
-    }
-    #[test]
-    fn test_resolve_single_entry() {
-        let dut = create_dut(vec![EndPoint::FileUnknown("testpath".into())]);
-        let ep = dut.retrieve(&"[[testpath]]".into()).unwrap();
-
-        assert!(matches!(ep, EndPoint::FileUnknown(path) if path==PathBuf::from("testpath")));
-    }
-
-    #[test]
-    fn test_new_correct_iteration() {
-        let test_ep_list: Vec<EndPoint> = vec![EndPoint::FileUnknown("testpath".into())];
-
-        let mut mock_res_id_res = MockResourceIdResolver::new();
-        mock_res_id_res
-            .expect_retrieve()
-            .returning(|_f| Ok(ResourceId("[[doesnt matter]]".to_string())));
-
-        let _dut = EndpointResourceIdMap::new(test_ep_list.iter(), &mock_res_id_res);
-    }
-
-    #[test]
-    fn test_new_resolve() {
-        let test_ep_list: Vec<EndPoint> = vec![EndPoint::FileUnknown("testpath".into())];
-
-        let mut mock_res_id_res = MockResourceIdResolver::new();
-        mock_res_id_res
-            .expect_retrieve()
-            .withf(|f| matches!(f, EndPoint::FileUnknown(path) if path==&PathBuf::from("testpath")))
-            .returning(|_f| Ok(ResourceId("[[doesnt matter]]".to_string())));
-
-        let _dut = EndpointResourceIdMap::new(test_ep_list.iter(), &mock_res_id_res);
-    }
-    */
 }
