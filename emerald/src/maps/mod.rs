7ecf232fe3125dddc9ac1d1b46a15738fecfe048
Christian Gröling <contact@christiangroeling.de>
2023-10-22 10:37:13 +0200

    Refactoring

--- emerald/src/maps/mod.rs
+++ emerald/src/maps/mod.rs
@@ -5,9 +5,9 @@ mod src_links_map;
 mod tgt_iter_retriever;
 mod tgt_links_map;
 
-pub use self::resource_id_link_map::ResourceIdLinkMap;
-pub use self::resource_id_resolver::ResourceIdResolver;
-pub use self::src_iter_retriever::SrcIterRetriever;
-pub use self::src_links_map::SrcLinksMap;
-pub use self::tgt_iter_retriever::TgtIterRetriever;
-pub use self::tgt_links_map::TgtLinksMap;
+pub use resource_id_link_map::ResourceIdLinkMap;
+pub use resource_id_resolver::ResourceIdResolver;
+pub use src_iter_retriever::SrcIterRetriever;
+pub use src_links_map::SrcLinksMap;
+pub use tgt_iter_retriever::TgtIterRetriever;
+pub use tgt_links_map::TgtLinksMap;
