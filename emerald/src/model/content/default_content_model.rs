2e1b57e7c1b2c1db735a5c43c4fbd778411a960e
Christian Gröling <contact@christiangroeling.de>
2023-10-28 20:24:06 +0200

    Refactoring

--- emerald/src/model/content/default_content_model.rs
+++ emerald/src/model/content/default_content_model.rs
@@ -4,19 +4,19 @@ use crate::types;
 
 #[derive(Clone)]
 pub struct DefaultContentModel {
-    content_map: MdContentMap,
+    md_content_map: MdContentMap,
 }
 
 impl DefaultContentModel {
     pub fn new(it_src: impl IntoIterator<Item = (types::ResourceId, types::Content)>) -> Self {
         Self {
-            content_map: MdContentMap::new(it_src),
+            md_content_map: MdContentMap::new(it_src),
         }
     }
 }
 
 impl MdContentRetriever for DefaultContentModel {
     fn retrieve(&self, rid: &types::ResourceId) -> &types::Content {
-        self.content_map.retrieve(rid)
+        self.md_content_map.retrieve(rid)
     }
 }
