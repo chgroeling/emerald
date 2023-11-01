e5eaebeac89d48895d4a6cc9cccd66baada6d319
Christian Gröling <contact@christiangroeling.de>
2023-11-01 10:03:30 +0100

    Refactoring

--- emerald/src/resources/file_meta_data_loader.rs
+++ emerald/src/resources/file_meta_data_loader.rs
@@ -4,6 +4,8 @@ use super::resource_object_retriever::ResourceObjectRetriever;
 use crate::error::{EmeraldError::*, Result};
 use crate::{types, EmeraldError};
 use chrono::prelude::*;
+#[cfg(test)]
+use mockall::{predicate::*, *};
 use std::fs;
 use std::marker::PhantomData;
 use std::path::Path;
@@ -12,15 +14,24 @@ use std::time::UNIX_EPOCH;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
+#[cfg_attr(test, automock)]
 pub trait FsMetaData {
-    fn get_meta_data_from_fs(path: &Path) -> Result<fs::Metadata>;
+    fn get_meta_data_from_fs(path: &Path) -> Result<(u64, u64)>;
 }
 pub struct FsMetaDataImpl();
 
 impl FsMetaDataImpl {
-    pub fn get_meta_data_from_fs(path: &Path) -> Result<fs::Metadata> {
+    pub fn get_meta_data_from_fs(path: &Path) -> Result<(u64, u64)> {
         if let Ok(meta_data) = fs::metadata(path) {
-            Ok(meta_data)
+            let modified = meta_data.modified()?;
+            let modified_dur = modified.duration_since(UNIX_EPOCH).unwrap();
+            let modified_u64 = modified_dur.as_secs();
+
+            let created = meta_data.created()?;
+            let created_dur = created.duration_since(UNIX_EPOCH).unwrap();
+            let created_u64 = created_dur.as_secs();
+
+            Ok((modified_u64, created_u64))
         } else {
             Err(EmeraldError::NoMetaData)
         }
@@ -28,7 +39,7 @@ impl FsMetaDataImpl {
 }
 
 impl FsMetaData for FsMetaDataImpl {
-    fn get_meta_data_from_fs(path: &Path) -> Result<fs::Metadata> {
+    fn get_meta_data_from_fs(path: &Path) -> Result<(u64, u64)> {
         Self::get_meta_data_from_fs(path)
     }
 }
@@ -75,19 +86,9 @@ where
 
     fn get_times(&self, path: &Path) -> Result<String> {
         let metadata = U::get_meta_data_from_fs(path)?;
+        let dt2 = Local.timestamp_opt(metadata.0 as i64, 0);
+        println!("// {dt2:?}");
 
-        let modified = metadata.modified()?;
-        let dur = modified.duration_since(UNIX_EPOCH).unwrap();
-        let dur_u64 = dur.as_secs();
-
-        let dt: DateTime<Local> = modified.into();
-
-        let dt2 = Local.timestamp_opt(dur_u64 as i64, 0);
-        if let Ok(time) = metadata.modified() {
-            println!("{dt:?} // {dt2:?}");
-        } else {
-            println!("Not supported on this platform");
-        }
         Ok("cdc".into())
     }
 
@@ -121,31 +122,41 @@ where
 }
 
 pub type FileMetaDataLoader<I> = FileMetaDataLoaderImpl<I, FsMetaDataImpl>;
+
 #[cfg(test)]
 mod tests {
-    use super::FileMetaDataLoader;
+    use super::FileMetaDataLoaderImpl;
+    use super::MockFsMetaData;
     use crate::resources::resource_object::ResourceObject;
     use crate::resources::{MetaDataLoader, MockResourceObjectRetriever};
     use crate::types;
     use std::path::PathBuf;
 
-    fn create_test_case(path: PathBuf) -> FileMetaDataLoader<MockResourceObjectRetriever> {
+    fn create_test_case(
+        path: PathBuf,
+    ) -> FileMetaDataLoaderImpl<MockResourceObjectRetriever, MockFsMetaData> {
         let mut mock = MockResourceObjectRetriever::new();
         mock.expect_retrieve()
             .returning(move |_f| Ok(ResourceObject::File(path.clone())));
-        FileMetaDataLoader::new(mock)
+
+        FileMetaDataLoaderImpl::new(mock)
     }
 
     #[test]
     fn test_load_file_type_is_markdown() {
         let dut = create_test_case("test.md".into());
+        let ctx = MockFsMetaData::get_meta_data_from_fs_context();
+        ctx.expect().returning(|_| Ok((0, 0)));
+
         let res = dut.load(&types::ResourceId::from("resid0")).unwrap();
-        assert_eq!(res.file_type, types::FileType::Markdown("md".into()))
+        assert_eq!(res.file_type, types::FileType::Markdown("md".into()));
     }
 
     #[test]
     fn test_load_file_type_is_no_file_type() {
         let dut = create_test_case("test".into());
+        let ctx = MockFsMetaData::get_meta_data_from_fs_context();
+        ctx.expect().returning(|_| Ok((0, 0)));
         let res = dut.load(&types::ResourceId::from("resid0")).unwrap();
         assert_eq!(res.file_type, types::FileType::NoFileType())
     }
