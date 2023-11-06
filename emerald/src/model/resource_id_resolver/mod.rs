d6f535f6c2684385c4f9aa10cb825e3bc952aed5
Christian Gröling <contact@christiangroeling.de>
2023-11-06 11:39:08 +0100

    Removed defects

--- emerald/src/model/resource_id_resolver/mod.rs
+++ emerald/src/model/resource_id_resolver/mod.rs
@@ -1,6 +1,6 @@
 mod default_link_resolver_model;
 mod resource_id_link_map;
-mod resource_id_resolver;
+mod resource_id_resolver_trait;
 
 pub use default_link_resolver_model::DefaultResourceIdResolverModel;
-pub use resource_id_resolver::ResourceIdResolver;
+pub use resource_id_resolver_trait::ResourceIdResolver;
