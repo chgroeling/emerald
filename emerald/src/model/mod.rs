f07115fed33b1659ce9734d69ad1d0fcbd7aeed1
Christian Gröling <contact@christiangroeling.de>
2023-11-06 07:52:52 +0100

    Refactoring

--- emerald/src/model/mod.rs
+++ emerald/src/model/mod.rs
@@ -1,5 +1,5 @@
 pub mod content;
-pub mod file;
 pub mod link;
 pub mod link_resolver;
 pub mod note;
+pub mod resource;
--- emerald/src/stats/extract_file_stats.rs
+++ emerald/src/stats/extract_file_stats.rs
@@ -1,12 +1,12 @@
 use super::vault_file_stats::VaultFileStats;
-use crate::model::file;
 use crate::model::note;
+use crate::model::resource;
 
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 
 pub fn extract_file_stats(
-    all_res_ids: &impl file::FileCount,
+    all_res_ids: &impl resource::ResourceCount,
     md_res_ids: &impl note::NoteCount,
 ) -> VaultFileStats {
     VaultFileStats {
