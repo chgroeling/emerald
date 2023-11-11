f0b2c4258da2a303d1e1fbaec1d2ad7258392b83
Christian Gröling <contact@christiangroeling.de>
2023-11-11 15:13:30 +0100

    Refactoring

--- emld/src/print_table.rs
+++ emld/src/print_table.rs
@@ -37,8 +37,6 @@ const TABLE_DEF: &[TableRow] = &[
     },
 ];
 
-const TRAIL: &str = "...";
-
 fn note_element_2_str(note: &Note, vault: &impl Vault, element: &str) -> String {
     match element {
         "title" => note.title.clone(),
@@ -61,38 +59,23 @@ fn note_element_2_str(note: &Note, vault: &impl Vault, element: &str) -> String 
     }
 }
 
-fn truncate_string(inp: &str, max_len: usize, trailing_str: &str) -> String {
-    if inp.len() > max_len {
-        let max_len_with_trail = max_len - trailing_str.len();
-        // unicode comptabitle split
-        let part1: String = inp.chars().take(max_len_with_trail).collect();
-        format!("{0}{1}", part1, trailing_str)
-    } else {
-        inp.to_string()
-    }
-}
-
-fn format_cell(content: &str, max_width: usize, trailing_str: &str) -> String {
-    let trimed_content = truncate_string(content, max_width, trailing_str);
-    format!("{0:<1$}", trimed_content, max_width)
-}
-
 pub fn print_table(vault: &impl Vault) {
     let expr_parser = ExprParser::new();
-    let format_string = "|%(title)|%(modified)|%(created)|%(size)|%(linkcnt)|%(backlinkcnt)|";
+    let format_string =
+        "|%<(30,trunc)%(title)|%<(19,trunc)%(modified)|%<(19,trunc)%(created)|%<(12,trunc)%(size)|%<(6,trunc)%(linkcnt)|%<(6,trunc)%(backlinkcnt)|";
 
     // print header
     let mut key_value_store = HashMap::<&str, String>::new();
     TABLE_DEF.iter().for_each(|cell_def| {
-        let out_str = format_cell(cell_def.element, cell_def.max_width, TRAIL);
-        key_value_store.insert(cell_def.element, out_str);
+        let out_str = cell_def.element;
+        key_value_store.insert(cell_def.element, out_str.to_string());
     });
     println!("{}", expr_parser.parse(&key_value_store, format_string));
 
     // print separator
     TABLE_DEF.iter().for_each(|cell_def| {
         let bar = "=".repeat(cell_def.max_width);
-        let out_str = format_cell(&bar, cell_def.max_width, TRAIL);
+        let out_str = bar;
         key_value_store.insert(cell_def.element, out_str);
     });
     println!("{}", expr_parser.parse(&key_value_store, format_string));
@@ -102,7 +85,7 @@ pub fn print_table(vault: &impl Vault) {
     for i in vault.flat_iter() {
         TABLE_DEF.iter().for_each(|cell_def| {
             let ref_cell = note_element_2_str(&i, vault, cell_def.element);
-            let out_str = format_cell(&ref_cell, cell_def.max_width, TRAIL);
+            let out_str = ref_cell;
             key_value_store.insert(cell_def.element, out_str);
         });
         println!("{}", expr_parser.parse(&key_value_store, format_string));
