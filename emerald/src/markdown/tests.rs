5130a6df819d997048eb39e670e93efa42581224
Christian Gröling <contact@christiangroeling.de>
2023-12-10 10:07:09 +0100

    refactor: Removed detect_two_more_dashes

--- emerald/src/markdown/tests.rs
+++ emerald/src/markdown/tests.rs
@@ -477,4 +477,17 @@ mod tests {
 
         assert_eq!(out_vec, [YamlFrontmatter("---\nyaml: true\n---\n".into())]);
     }
+
+    #[test]
+    fn test_iter_with_illegal_yaml_frontmatter_illegal_whitespaces() {
+        let test_str = "--- \n\
+        yaml: true\n\
+        ---\n\
+        Text
+        ";
+        let output = MarkdownAnalyzerIter::new(&test_str);
+        let out_vec: Vec<_> = output.collect();
+
+        assert_eq!(out_vec, []);
+    }
 }
