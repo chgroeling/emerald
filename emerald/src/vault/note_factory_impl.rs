08c559b27a2725cd66a0a610768ff117d7396904
Christian Gröling <contact@christiangroeling.de>
2023-12-17 10:51:59 +0100

    refactor: Removed superflous borrow

--- emerald/src/vault/note_factory_impl.rs
+++ emerald/src/vault/note_factory_impl.rs
@@ -30,7 +30,7 @@ impl NoteFactory for NoteFactoryImpl {
         let content = self.content_retriever.retrieve(&rid);
         let markdown_splitter = MarkdownFrontMatterSplitter::new();
 
-        let (yaml_str, markdown) = markdown_splitter.split(&content);
+        let (yaml_str, markdown) = markdown_splitter.split(content);
 
         Note::new(
             rid,
