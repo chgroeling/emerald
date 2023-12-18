18137c4a41f9d3b3525c10ffe51853d94f57cc84
Christian Gröling <contact@christiangroeling.de>
2023-12-18 13:42:12 +0100

    refactor: Separate inline_codeblock_state

--- emerald/src/markdown/markdown_analyzer_iter.rs
+++ emerald/src/markdown/markdown_analyzer_iter.rs
@@ -1,6 +1,5 @@
 use super::markdown_iterator_state::{ActionResult, State, StateData, Yield};
 use super::states;
-use super::utils::*;
 use crate::types;
 
 #[allow(unused_imports)]
@@ -20,50 +19,6 @@ impl<'a> MarkdownAnalyzerIter<'a> {
                 state: State::DocumentStart,
                 it: buf.char_indices().peekable(),
             },
-        }
-    }
-
-    fn detect_inline_code_block(state_data: &mut StateData, start_idx: usize) -> ActionResult {
-        let open_cnt = gather!(state_data.it, Option::<i32>::None, ' ');
-
-        if open_cnt < 4 {
-            return ActionResult::NextState(State::EmptyLine);
-        }
-
-        // Opening Inline Code Block was detected
-        let mut act_idx = start_idx + open_cnt as usize - 1; // -1 because the first char doesn't count
-
-        loop {
-            // end of file detection
-            let ConsumeResult::Some((idx, i)) = consume!(state_data.it) else {
-                break;
-            };
-
-            if i == '\n' {
-                return ActionResult::YieldState(
-                    State::InlCodeBlock,
-                    Yield::CodeBlock(start_idx, idx),
-                );
-            }
-
-            act_idx = idx;
-        }
-
-        // end of file handling
-        ActionResult::YieldState(
-            State::InlCodeBlock,
-            Yield::CodeBlock(start_idx, act_idx + 1),
-        )
-    }
-
-    fn inline_codeblock_state(state_data: &mut StateData) -> ActionResult {
-        let Some((index, i)) = state_data.it.peek().cloned() else {
-            return ActionResult::EndOfFile;
-        };
-
-        match i {
-            ' ' => Self::detect_inline_code_block(state_data, index),
-            _ => ActionResult::NextState(State::Text),
         }
     }
 
@@ -87,7 +42,7 @@ impl<'a> Iterator for MarkdownAnalyzerIter<'a> {
                 State::EmptyLine => states::empty_line(&mut self.state_data),
                 State::NewLine => states::new_line(&mut self.state_data),
                 State::YamlFrontmatter => states::yaml_frontmatter(&mut self.state_data),
-                State::InlCodeBlock => Self::inline_codeblock_state(&mut self.state_data),
+                State::InlCodeBlock => states::inline_codeblock(&mut self.state_data),
                 State::Text => states::text(&mut self.state_data),
             };
 
