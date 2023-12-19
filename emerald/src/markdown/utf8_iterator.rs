07cdeaa3422e52c10c415f809cf9f945439bb7fd
Christian Gröling <contact@christiangroeling.de>
2023-12-19 11:16:40 +0100

    refactor: Optimize utf8 iterator.

--- emerald/src/markdown/utf8_iterator.rs
+++ emerald/src/markdown/utf8_iterator.rs
@@ -1,7 +1,6 @@
 #[derive(Debug)]
 pub struct Utf8Iterator<'a> {
     slice: &'a str,
-    slice_len: usize,
     byte_pos: usize,
 
     next_char: Option<(usize, char)>, // Cache for the next character
@@ -12,32 +11,37 @@ impl<'a> Utf8Iterator<'a> {
         let mut iterator = Utf8Iterator {
             slice,
             byte_pos: 0,
-            slice_len: slice.len(),
             next_char: None,
         };
         iterator.next_char = iterator.peek_next();
         iterator
     }
 
+    fn char_len(&self, pos: usize) -> usize {
+        let first_byte = self.slice.as_bytes()[pos];
+        match first_byte {
+            0x00..=0x7F => 1,
+            0xC0..=0xDF => 2,
+            0xE0..=0xEF => 3,
+            0xF0..=0xF7 => 4,
+            _ => panic!("Invalid UTF-8 start byte"),
+        }
+    }
     fn peek_next(&self) -> Option<(usize, char)> {
-        if self.byte_pos < self.slice_len {
-            let first_byte = self.slice.as_bytes()[self.byte_pos];
-            let char_len = match first_byte {
-                0x00..=0x7F => return Some((self.byte_pos, first_byte as char)),
-                0xC0..=0xDF => 2,
-                0xE0..=0xEF => 3,
-                0xF0..=0xF7 => 4,
-                _ => panic!("Invalid UTF-8 start byte"),
-            };
+        if self.byte_pos < self.slice.len() {
+            let char_len = self.char_len(self.byte_pos);
+            if char_len == 1 {
+                Some((self.byte_pos, self.slice.as_bytes()[self.byte_pos] as char))
+            } else {
+                let char_bytes = &self.slice.as_bytes()[self.byte_pos..self.byte_pos + char_len];
+                let char = std::str::from_utf8(char_bytes)
+                    .unwrap()
+                    .chars()
+                    .next()
+                    .unwrap();
 
-            let char_bytes = &self.slice.as_bytes()[self.byte_pos..self.byte_pos + char_len];
-            let char = std::str::from_utf8(char_bytes)
-                .unwrap()
-                .chars()
-                .next()
-                .unwrap();
-
-            Some((self.byte_pos, char))
+                Some((self.byte_pos, char))
+            }
         } else {
             None
         }
@@ -53,7 +57,7 @@ impl<'a> Iterator for Utf8Iterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some((pos, ch)) = self.next_char.take() {
-            self.byte_pos = pos + ch.len_utf8();
+            self.byte_pos = pos + self.char_len(self.byte_pos);
             self.next_char = self.peek_next();
             Some((pos, ch))
         } else {
