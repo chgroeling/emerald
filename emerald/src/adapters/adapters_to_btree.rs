5bb42c824d320490c0b4df7cab59c90634340fea
Christian Gröling <contact@christiangroeling.de>
2023-12-22 12:22:05 +0100

    feat: Export Frontmatter

--- emerald/src/adapters/adapters_to_btree.rs
+++ emerald/src/adapters/adapters_to_btree.rs
@@ -2,10 +2,9 @@ use crate::types;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Frontmatter {
+pub struct Frontmatter {
     tags: Option<String>,
     aliases: Option<Vec<String>>,
     created: Option<String>,
@@ -15,20 +14,16 @@ struct Frontmatter {
 
 pub fn adapter_to_btree<'a>(
     it_src: impl IntoIterator<Item = (&'a types::ResourceId, &'a str)> + 'a,
-) -> impl Iterator<Item = (&'a types::ResourceId, BTreeMap<String, String>)> + 'a {
+) -> impl Iterator<Item = (&'a types::ResourceId, Frontmatter)> + 'a {
     let it = it_src.into_iter().filter_map(|f| {
         let res = serde_yaml::from_str::<Frontmatter>(f.1);
         match res {
-            Ok(yaml_meta_data) => Some(yaml_meta_data),
+            Ok(yaml_meta_data) => Some((f.0, yaml_meta_data)),
             Err(err) => {
                 warn!("Invalid yaml found in {:?}\nError: {}\n{}", f.0, err, f.1);
                 None
             }
         }
     });
-
-    for i in it {
-        //   println!("{:?}", i)
-    }
-    std::iter::empty::<(&'a types::ResourceId, BTreeMap<String, String>)>()
+    it
 }
