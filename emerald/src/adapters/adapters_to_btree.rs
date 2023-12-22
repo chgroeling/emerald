4b6d204d3a82435fe8d3cae30750ffc532b9b856
Christian Gröling <contact@christiangroeling.de>
2023-12-22 08:32:34 +0100

    feat: Frontmatter iterator

--- emerald/src/adapters/adapters_to_btree.rs
+++ emerald/src/adapters/adapters_to_btree.rs
@@ -1,14 +1,19 @@
-use super::adapters_to_link_2_tgt::adapter_to_link_2_tgt;
-use super::adapters_to_links::adapter_to_rid_and_links;
-use crate::{model::resource_id_resolver, types};
+use crate::markdown;
+use crate::types;
 
-pub fn adapter_to_btree<'a>(
-    it_src: impl IntoIterator<Item = (&'a types::ResourceId, types::MdBlock<'a>)> + 'a,
-    rid_resolver: &'a impl resource_id_resolver::ResourceIdResolver,
-) -> impl Iterator<Item = types::LinkSrc2Tgt> + 'a {
-    let it1 = adapter_to_rid_and_links(it_src);
-    let it2 = adapter_to_link_2_tgt(it1, rid_resolver);
+pub fn adapter_to_btree<'a, I: markdown::MarkdownAnalyzer<'a> + 'a + Copy>(
+    it_src: impl IntoIterator<Item = (&'a types::ResourceId, &'a types::Content)> + 'a,
+    md_analyzer: I,
+) -> impl Iterator<Item = (&'a types::ResourceId, &'a str)> + 'a {
+    let it1 = it_src.into_iter().filter_map(move |f| {
+        let first_element = md_analyzer.analyze(&f.1 .0).next();
+        if let Some(types::MdBlock::YamlFrontmatter(yaml)) = first_element {
+            // markdown starts when yaml ends
+            Some((f.0, yaml))
+        } else {
+            None
+        }
+    });
 
-    it2.into_iter()
-        .map(|(rid, link_2_tgt)| types::LinkSrc2Tgt::from_link_to_target(rid.clone(), link_2_tgt))
+    it1
 }
