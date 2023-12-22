be830b8a58bcd2b6568fc89a5f4593e34f3dde4a
Christian Gröling <contact@christiangroeling.de>
2023-12-22 14:55:28 +0100

    refactor: Rename to adapters_to_rid_and_filesystem_meta_data

--- emerald/src/adapters/mod.rs
+++ emerald/src/adapters/mod.rs
@@ -6,7 +6,7 @@ mod adapters_to_name;
 mod adapters_to_rid;
 mod adapters_to_rid_and_content;
 mod adapters_to_rid_and_content_type;
-mod adapters_to_rid_and_meta_data;
+mod adapters_to_rid_and_filesystem_meta_data;
 mod adapters_to_yaml;
 
 pub use adapters_to_btree::adapter_to_btree;
@@ -15,5 +15,5 @@ pub use adapters_to_name::adapter_to_rid_and_name;
 pub use adapters_to_rid::filter_rid_and_meta_data;
 pub use adapters_to_rid_and_content::adapter_to_rids_and_content;
 pub use adapters_to_rid_and_content_type::adapter_to_rid_and_content_type;
-pub use adapters_to_rid_and_meta_data::adapter_to_rid_and_meta_data;
+pub use adapters_to_rid_and_filesystem_meta_data::adapter_to_rid_and_filesystem_meta_data;
 pub use adapters_to_yaml::adapter_to_yaml;
