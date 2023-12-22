7c83de7304f044c6d19d194347028fbb95ea3244
Christian Gröling <contact@christiangroeling.de>
2023-12-22 11:14:10 +0100

    feat: Add frontmatter struct for deserialization

--- emerald/src/adapters/adapters_to_btree.rs
+++ emerald/src/adapters/adapters_to_btree.rs
@@ -1,13 +1,23 @@
 use crate::types;
 #[allow(unused_imports)]
 use log::{debug, error, info, trace, warn};
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Frontmatter {
+    tags: String,
+    aliases: Vec<String>,
+    created: String,
+    modified: String,
+    keywords: Option<String>,
+}
 
 pub fn adapter_to_btree<'a>(
     it_src: impl IntoIterator<Item = (&'a types::ResourceId, &'a str)> + 'a,
 ) -> impl Iterator<Item = (&'a types::ResourceId, BTreeMap<String, String>)> + 'a {
     let it = it_src.into_iter().filter_map(|f| {
-        let res = serde_yaml::from_str::<BTreeMap<String, String>>(f.1);
+        let res = serde_yaml::from_str::<Frontmatter>(f.1);
         match res {
             Ok(yaml_meta_data) => Some(yaml_meta_data),
             Err(err) => {
