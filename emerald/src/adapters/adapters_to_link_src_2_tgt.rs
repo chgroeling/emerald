15e91cd0a2157edefa5a4d573d44f90a19b7042e
Christian Gröling <contact@christiangroeling.de>
2023-12-22 15:06:10 +0100

    refactor: Rename to adapter_to_rid_and_link_2_tgt

--- emerald/src/adapters/adapters_to_link_src_2_tgt.rs
+++ emerald/src/adapters/adapters_to_link_src_2_tgt.rs
@@ -1,4 +1,4 @@
-use super::adapters_to_link_2_tgt::adapter_to_link_2_tgt;
+use super::adapters_to_rid_and_link_2_tgt::adapter_to_rid_and_link_2_tgt;
 use super::adapters_to_rid_and_links::adapter_to_rid_and_links;
 use crate::{model::resource_id_resolver, types};
 
@@ -7,7 +7,7 @@ pub fn adapter_to_link_src_2_tgt<'a>(
     rid_resolver: &'a impl resource_id_resolver::ResourceIdResolver,
 ) -> impl Iterator<Item = types::LinkSrc2Tgt> + 'a {
     let it1 = adapter_to_rid_and_links(it_src);
-    let it2 = adapter_to_link_2_tgt(it1, rid_resolver);
+    let it2 = adapter_to_rid_and_link_2_tgt(it1, rid_resolver);
 
     it2.into_iter()
         .map(|(rid, link_2_tgt)| types::LinkSrc2Tgt::from_link_to_target(rid.clone(), link_2_tgt))
