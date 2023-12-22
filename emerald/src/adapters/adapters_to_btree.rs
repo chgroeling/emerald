1319e92a06dbce62da519df9e549a243506f39b9
Christian Gröling <contact@christiangroeling.de>
2023-12-22 10:40:23 +0100

    feat: Add conversion to btree

--- emerald/src/adapters/adapters_to_btree.rs
+++ emerald/src/adapters/adapters_to_btree.rs
@@ -1,9 +1,24 @@
-use std::collections::BTreeMap;
-
 use crate::types;
+#[allow(unused_imports)]
+use log::{debug, error, info, trace, warn};
+use std::collections::BTreeMap;
 
 pub fn adapter_to_btree<'a>(
     it_src: impl IntoIterator<Item = (&'a types::ResourceId, &'a str)> + 'a,
 ) -> impl Iterator<Item = (&'a types::ResourceId, BTreeMap<String, String>)> + 'a {
+    let it = it_src.into_iter().filter_map(|f| {
+        let res = serde_yaml::from_str::<BTreeMap<String, String>>(f.1);
+        match res {
+            Ok(yaml_meta_data) => Some(yaml_meta_data),
+            Err(err) => {
+                warn!("Invalid yaml found in {:?}\nError: {}\n{}", f.0, err, f.1);
+                None
+            }
+        }
+    });
+
+    for i in it {
+        //   println!("{:?}", i)
+    }
     std::iter::empty::<(&'a types::ResourceId, BTreeMap<String, String>)>()
 }
