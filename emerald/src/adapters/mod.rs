893a953abe3c615c9a67f5e7109099aea5d0badc
Christian Gröling <contact@christiangroeling.de>
2023-12-22 15:18:57 +0100

    refactor: Rename to adapter_to_rid_and_yaml

--- emerald/src/adapters/mod.rs
+++ emerald/src/adapters/mod.rs
@@ -7,7 +7,7 @@ mod adapters_to_rid_and_filesystem_metadata;
 mod adapters_to_rid_and_link_2_tgt;
 mod adapters_to_rid_and_links;
 mod adapters_to_rid_and_name;
-mod adapters_to_yaml;
+mod adapters_to_rid_and_yaml;
 
 pub use adapters_to_link_src_2_tgt::adapter_to_link_src_2_tgt;
 pub use adapters_to_rid::filter_rid_and_meta_data;
@@ -16,4 +16,4 @@ pub use adapters_to_rid_and_content_type::adapter_to_rid_and_content_type;
 pub use adapters_to_rid_and_document_metadata::adapter_to_rid_and_document_metadata;
 pub use adapters_to_rid_and_filesystem_metadata::adapter_to_rid_and_filesystem_metadata;
 pub use adapters_to_rid_and_name::adapter_to_rid_and_name;
-pub use adapters_to_yaml::adapter_to_yaml;
+pub use adapters_to_rid_and_yaml::adapter_to_rid_and_yaml;
