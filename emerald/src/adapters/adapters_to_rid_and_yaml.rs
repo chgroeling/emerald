efa189b24b5bae2a7b8cfe5547a5323ef5b89936
Christian Gröling <contact@christiangroeling.de>
2023-12-23 05:30:07 +0100

    refactor: Do not exclude empty yaml

--- emerald/src/adapters/adapters_to_rid_and_yaml.rs
+++ emerald/src/adapters/adapters_to_rid_and_yaml.rs
@@ -5,13 +5,13 @@ pub fn adapter_to_rid_and_yaml<'a, I: markdown::MarkdownAnalyzer<'a> + 'a + Copy
     it_src: impl IntoIterator<Item = (&'a types::ResourceId, &'a types::Content)> + 'a,
     md_analyzer: I,
 ) -> impl Iterator<Item = (&'a types::ResourceId, &'a str)> + 'a {
-    let it1 = it_src.into_iter().filter_map(move |f| {
+    let it1 = it_src.into_iter().map(move |f| {
         let first_element = md_analyzer.analyze(&f.1 .0).next();
         if let Some(types::MdBlock::YamlFrontmatter(yaml)) = first_element {
             // markdown starts when yaml ends
-            Some((f.0, yaml))
+            (f.0, yaml)
         } else {
-            None
+            (f.0, "")
         }
     });
 
