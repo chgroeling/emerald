31e77cb665bfd9d0eded74a7096066f9d1c2e0ae
Christian Gröling <contact@christiangroeling.de>
2023-12-24 08:26:10 +0100

    refactor: Extract from trait

---
+++ emerald/src/model/note/note_metadata_from.rs
@@ -0,0 +1,27 @@
+use super::note_metadata::{DocumentMetadata, FilesystemMetadata, NoteMetadata};
+use crate::types;
+
+impl From<(types::FilesystemMetadata, types::DocumentMetadata)> for NoteMetadata {
+    fn from(value: (types::FilesystemMetadata, types::DocumentMetadata)) -> Self {
+        let fs_md = FilesystemMetadata {
+            location: value.0.location,
+            size: value.0.size,
+            modified: value.0.modified,
+            created: value.0.created,
+        };
+
+        let doc_md = DocumentMetadata {
+            aliases: value.1.aliases.unwrap_or(vec![]),
+            keywords: value.1.keywords.unwrap_or(vec![]),
+            modified: value.1.modified,
+            created: value.1.created,
+            tags: value.1.tags,
+        };
+
+        Self {
+            title: value.0.name,
+            filesystem: fs_md,
+            document: doc_md,
+        }
+    }
+}
