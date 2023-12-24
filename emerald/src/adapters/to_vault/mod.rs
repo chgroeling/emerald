039700ba2f0e46f325721313168d432172dd0a24
Christian Gröling <contact@christiangroeling.de>
2023-12-24 11:52:12 +0100

    refactor: Move NoteMetadatatriever

--- emerald/src/adapters/to_vault/mod.rs
+++ emerald/src/adapters/to_vault/mod.rs
@@ -4,8 +4,10 @@ mod filesystem_metadata_from;
 mod get_backlinks_impl;
 mod get_links_impl;
 mod link_query_result_builder;
+mod note_metadata_retriever_adapter;
 mod resource_id_from;
 
 pub use content_retriever_adapter::ContentRetrieverAdapter;
 pub use get_backlinks_impl::GetBacklinksImpl;
 pub use get_links_impl::GetLinksImpl;
+pub use note_metadata_retriever_adapter::NoteMetadataRetrieverAdapter;
