b717e51e4a5a4ce94d66733b6344d4f9e2d5e3f4
Christian Gröling <contact@christiangroeling.de>
2023-12-24 11:42:17 +0100

    feat: Adapt Content retriever in vault requirements

--- emerald/src/model/vault/note_factory_impl.rs
+++ emerald/src/model/vault/note_factory_impl.rs
@@ -2,7 +2,6 @@ use super::note_factory::NoteFactory;
 use super::resource_id::ResourceId;
 use super::{MdContentRetriever, Note, NoteMetadataRetriever};
 use crate::markdown::MarkdownFrontMatterSplitter;
-use crate::types;
 use std::rc::Rc;
 
 #[derive(Clone)]
@@ -26,9 +25,7 @@ impl NoteFactoryImpl {
 impl NoteFactory for NoteFactoryImpl {
     fn create_note(&self, rid: ResourceId) -> Note {
         let (title, filesystem_md, document_md) = self.metadata_retriever.retrieve(&rid);
-
-        let rid_conv: types::ResourceId = rid.clone().into();
-        let content = self.content_retriever.retrieve(&rid_conv);
+        let content = self.content_retriever.retrieve(&rid);
         let markdown_splitter = MarkdownFrontMatterSplitter::new();
 
         let (yaml_str, markdown) = markdown_splitter.split(content);
