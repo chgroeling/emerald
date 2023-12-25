6774323e2931537bd2141cd0f28b4372ba15033b
Christian Gröling <contact@christiangroeling.de>
2023-12-25 08:33:39 +0100

    refactor: Remove superflous code

--- emerald/src/resources/mod.rs
+++ emerald/src/resources/mod.rs
@@ -1,4 +1,3 @@
-mod adapter_to_rid;
 mod adapter_to_ro;
 mod adapter_to_ro_and_rid;
 mod content_loader;
@@ -8,14 +7,12 @@ mod get_path_list;
 
 mod adapter_to_rid_and_content;
 mod filesystem_metadata_loader;
-mod resource_id_map;
 mod resource_id_retriever;
 mod resource_object;
 mod resource_object_map;
 mod resource_object_retriever;
 mod resource_object_translation;
 
-pub use adapter_to_rid::adapter_to_rid;
 pub use adapter_to_rid_and_content::adapter_to_rid_and_content;
 pub use adapter_to_ro::adapter_to_ro;
 pub use adapter_to_ro_and_rid::adapter_to_ro_and_rid;
@@ -25,7 +22,6 @@ pub use filesystem_metadata_loader::FilesystemMetadataLoader;
 pub use filesystem_metadata_loader_impl::FilesystemMetadataLoaderImpl;
 pub use filesystem_metadata_loader_impl::FsMetadataAccessImpl;
 pub use get_path_list::get_path_list;
-pub use resource_id_map::ResourceIdMap;
 pub use resource_object_map::ResourceObjectMap;
 
 #[cfg(test)]
