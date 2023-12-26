ab201da1a5e2f6611d5b03bdf9d7871b98df1a33
Christian Gröling <contact@christiangroeling.de>
2023-12-26 12:41:09 +0100

    feat: Remove location from filesystem meta data

--- emerald/src/model/vault/note/filesystem_metadata.rs
+++ emerald/src/model/vault/note/filesystem_metadata.rs
@@ -2,7 +2,7 @@ use super::timestamp::Timestamp;
 
 #[derive(Debug, Clone, PartialEq, Hash, Default)]
 pub struct FilesystemMetadata {
-    pub location: String,
+    pub path: String,
     pub size: u64,
     pub modified: Timestamp,
     pub created: Timestamp,
