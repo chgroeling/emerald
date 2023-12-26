847e266f6b5cd2dc7f2d07acffcb69d9dbbbdf7c
Christian Gröling <contact@christiangroeling.de>
2023-12-26 11:40:11 +0100

    feat: Resource Id now contains an immutable string

--- emerald/src/types/resource_id.rs
+++ emerald/src/types/resource_id.rs
@@ -4,18 +4,18 @@
 ///
 /// Currently a ResourceId is nothing else than a string containing a path
 /// to the filesystem
-pub struct ResourceId(pub String);
+pub struct ResourceId(pub Box<str>);
 
 // Allows to use a string as a ResourceId
 impl From<&str> for ResourceId {
     fn from(value: &str) -> Self {
-        Self(value.to_string())
+        Self(value.to_owned().into_boxed_str())
     }
 }
 
 impl From<String> for ResourceId {
     fn from(value: String) -> Self {
-        Self(value)
+        Self(value.into_boxed_str())
     }
 }
 
