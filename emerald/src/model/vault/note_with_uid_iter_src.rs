9d5aeba18e0726bf6285b0c3f55cc199d66b8607
Christian Gröling <contact@christiangroeling.de>
2023-12-27 10:58:26 +0100

    refactor: Rename to NoteWithUidIterSrc

--- emerald/src/model/vault/note_with_uid_iter_src.rs
+++ emerald/src/model/vault/note_with_uid_iter_src.rs
@@ -1,26 +1,28 @@
-use std::rc::Rc;
+use std::{cell::Cell, rc::Rc};
 
 use super::{NotesIterSrc, ResourceId};
 
-pub struct UidMapper<I>
+pub struct NoteWithUidIterSrc<I>
 where
     I: Iterator<Item = ResourceId>,
 {
     notes_iter_src: Rc<dyn NotesIterSrc<Iter = I>>,
+    next_uid: Cell<u32>,
 }
 
-impl<I> UidMapper<I>
+impl<I> NoteWithUidIterSrc<I>
 where
     I: Iterator<Item = ResourceId>,
 {
     pub fn new(iter_src: Rc<dyn NotesIterSrc<Iter = I>>) -> Self {
         Self {
             notes_iter_src: iter_src,
+            next_uid: Cell::new(0),
         }
     }
 }
 
-impl<I> NotesIterSrc for UidMapper<I>
+impl<I> NotesIterSrc for NoteWithUidIterSrc<I>
 where
     I: Iterator<Item = ResourceId>,
 {
@@ -29,6 +31,9 @@ where
     fn create_iter(&self) -> Self::Iter {
         let ret: Vec<_> = self.notes_iter_src.create_iter().collect();
 
+        let uid = self.next_uid.get() + 1;
+
+        self.next_uid.set(uid);
         ret.into_iter()
     }
 }
