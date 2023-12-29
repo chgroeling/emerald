53af71dc8d0122d3c882515a302f9c128221b2eb
Christian Gröling <contact@christiangroeling.de>
2023-12-29 14:15:04 +0100

    feat: Create another level of indirection with Uids

--- emerald/src/model/vault/uid_map.rs
+++ emerald/src/model/vault/uid_map.rs
@@ -3,9 +3,11 @@ use std::collections::HashMap;
 use super::uid::Uid;
 use super::ResourceId;
 
+#[derive(Debug, Clone)]
 pub struct UidMap {
     uid_to_rid: HashMap<Uid, ResourceId>,
     rid_to_uid: HashMap<ResourceId, Uid>,
+    next_uid: u32,
 }
 
 impl UidMap {
@@ -13,14 +15,23 @@ impl UidMap {
         Self {
             uid_to_rid: HashMap::new(),
             rid_to_uid: HashMap::new(),
+            next_uid: 0,
         }
     }
-    pub fn resource_id_from_uid(&self, uid: &Uid) -> ResourceId {
-        todo!();
+    pub fn get_rid_from_uid(&self, uid: &Uid) -> Option<&ResourceId> {
+        self.uid_to_rid.get(uid)
     }
 
-    pub fn get_or_assign_uid(&mut self, rid: &ResourceId) -> Uid {
-        //todo!();
-        Uid::default()
+    pub fn get_uid_from_rid(&self, rid: &ResourceId) -> Option<&Uid> {
+        self.rid_to_uid.get(rid)
+    }
+
+    pub fn assign_uid(&mut self, rid: &ResourceId) -> Uid {
+        let act_uid = self.next_uid;
+        let uid = Uid(act_uid.to_string().into_boxed_str());
+        self.rid_to_uid.insert(rid.clone(), uid.clone());
+        self.uid_to_rid.insert(uid.clone(), rid.clone());
+        self.next_uid += 1;
+        uid
     }
 }
