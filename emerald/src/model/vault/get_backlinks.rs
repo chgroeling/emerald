cee710abe34d6c957511e4a344390de0cbb6ac1e
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:40:07 +0100

    doc: Add rustdoc to links,backlinks,retriever

--- emerald/src/model/vault/get_backlinks.rs
+++ emerald/src/model/vault/get_backlinks.rs
@@ -1,5 +1,11 @@
 use super::{link_query_result::LinkQueryResult, ExResourceId};
 
+/// Trait for querying links pointing to a target resource.
 pub trait GetBacklinks {
+    /// Returns an iterator over links pointing to the specified resource.
+    ///
+    /// # Arguments
+    ///
+    /// * `rid`: Resource identifier.
     fn get_backlinks_of(&self, rid: &ExResourceId) -> Box<dyn Iterator<Item = LinkQueryResult>>;
 }
