1c9fb3a65a9ec6b2e8f50b4a1db794defdb20b1c
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:47:05 +0100

    rustdoc: Add uid specific rustdocs

--- emerald/src/model/vault/uid.rs
+++ emerald/src/model/vault/uid.rs
@@ -1,5 +1,12 @@
+/// Represents a unique identifier (UID).
+///
+/// This struct encapsulates a UID as a boxed string for efficient storage.
+
 #[derive(Debug, Clone, PartialEq, Hash, Default)]
 
 pub struct Uid(pub Box<str>);
 
+/// Enables equality comparisons for `Uid`.
+///
+/// `Uid` instances are considered equal if their inner boxed strings are equal.
 impl Eq for Uid {}
