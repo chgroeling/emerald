2818642277f10756816b0a32f55ee880cf5b3363
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:47:05 +0100

    rustdoc: Add uid specific rustdocs

--- emerald/src/model/vault/uid_map.rs
+++ emerald/src/model/vault/uid_map.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use super::uid::Uid;
 use super::ExResourceId;
 
+/// Manages mappings between UIDs (unique identifiers) and resource IDs.
 #[derive(Debug, Clone)]
 pub struct UidMap {
     uid_to_rid: HashMap<Uid, ExResourceId>,
@@ -11,6 +12,7 @@ pub struct UidMap {
 }
 
 impl UidMap {
+    /// Constructs a new `UidMap`.
     pub fn new() -> Self {
         Self {
             uid_to_rid: HashMap::new(),
@@ -18,14 +20,42 @@ impl UidMap {
             next_uid: 0,
         }
     }
+    /// Retrieves the resource ID associated with a given UID.
+    ///
+    /// # Arguments
+    ///
+    /// * `uid`: The UID to look up.
+    ///
+    /// # Returns
+    ///
+    /// Option containing the corresponding resource ID, if it exists.
+
     pub fn get_rid_from_uid(&self, uid: &Uid) -> Option<&ExResourceId> {
         self.uid_to_rid.get(uid)
     }
 
+    /// Retrieves the UID associated with a given resource ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `rid`: The resource ID to look up.
+    ///
+    /// # Returns
+    ///
+    /// Option containing the corresponding UID, if it exists.
     pub fn get_uid_from_rid(&self, rid: &ExResourceId) -> Option<&Uid> {
         self.rid_to_uid.get(rid)
     }
 
+    /// Assigns a new UID to the given resource ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `rid`: The resource ID to assign a UID.
+    ///
+    /// # Returns
+    ///
+    /// The new UID assigned to the resource ID.
     pub fn assign_uid(&mut self, rid: &ExResourceId) -> Uid {
         let act_uid = self.next_uid;
         let uid = Uid(act_uid.to_string().into_boxed_str());
--- emerald/src/model/vault/vault_trait.rs
+++ emerald/src/model/vault/vault_trait.rs
@@ -2,6 +2,18 @@ use super::{note_types::NoteTypes, Note};
 
 pub trait Vault {
     fn flat_iter(&self) -> std::vec::IntoIter<Note>;
+
+    /// Returns an iterator over links contained in the specified Note.
+    ///
+    /// # Arguments
+    ///
+    /// * `note`: Note.
     fn get_links_of(&self, note: &Note) -> Box<dyn Iterator<Item = NoteTypes>>;
+
+    /// Returns an iterator over links pointing to the specified Note.
+    ///
+    /// # Arguments
+    ///
+    /// * `note`: Note.
     fn get_backlinks_of(&self, note: &Note) -> Box<dyn Iterator<Item = NoteTypes>>;
 }
