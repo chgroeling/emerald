9ae2c46153920c23ed922c44688e03384fe0b575
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:24:32 +0100

    doc: Update doc of resource_log

--- emerald/src/model/resource_id_resolver/resource_loc.rs
+++ emerald/src/model/resource_id_resolver/resource_loc.rs
@@ -1,5 +1,15 @@
 use crate::types;
 
+/// Represents a location of a resource.
+///
+/// This struct holds a resource identifier, along with normalized file name
+/// and directory path information.
+///
+/// # Fields
+///
+/// * `rid`: Resource identifier (`types::ResourceId`).
+/// * `norm_filename`: Normalized file name (`Box<str>`).
+/// * `dir_path`: Path where the file resides (`Box<str>`).
 pub struct ResourceLoc {
     pub rid: types::ResourceId,
     pub norm_filename: Box<str>,
