d69e0d59f019aece0f4aaa114ea7cbf762d395db
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:50:29 +0100

    doc: Update docs in resource_id

--- emerald/src/types/resource_id.rs
+++ emerald/src/types/resource_id.rs
@@ -1,28 +1,33 @@
 #[derive(Debug, Clone, PartialEq, Hash)]
 
-/// A ResourceId points to a unique Resource
+/// Represents a unique identifier for a resource.
 ///
-/// Currently a ResourceId is nothing else than a string containing a path
-/// to the filesystem
+/// This struct is primarily used to represent resource identifiers as strings,
+/// specifically as paths in the filesystem.
 pub struct ResourceId(pub Box<str>);
 
-// Allows to use a string as a ResourceId
+/// Implements `From<&str>` for `ResourceId`, allowing conversion from string slice.
 impl From<&str> for ResourceId {
     fn from(value: &str) -> Self {
         Self(value.to_owned().into_boxed_str())
     }
 }
 
+/// Implements `From<String>` for `ResourceId`, allowing conversion from `String`.
 impl From<String> for ResourceId {
     fn from(value: String) -> Self {
         Self(value.into_boxed_str())
     }
 }
 
+/// Implements `From<Box<str>>` for `ResourceId`, allowing conversion from `Box<str>`.
 impl From<Box<str>> for ResourceId {
     fn from(value: Box<str>) -> Self {
         Self(value)
     }
 }
 
+/// Enables equality comparisons for `ResourceId`.
+///
+/// `ResourceId` instances are considered equal if their inner boxed strings are equal.
 impl Eq for ResourceId {}
