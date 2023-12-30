bd0fdeafe9a8b2a5212285dc0be5ac067996bb09
Christian Gröling <contact@christiangroeling.de>
2023-12-30 07:47:12 +0100

    feat: Do not use special resource id format

--- emerald/src/resources/resource_object_translation.rs
+++ emerald/src/resources/resource_object_translation.rs
@@ -1,6 +1,5 @@
 use super::resource_object::ResourceObject;
 use crate::types;
-use crate::utils;
 use std::path::Path;
 
 pub fn convert_ro_to_rid(ro: &ResourceObject, common_path: &Path) -> types::ResourceId {
@@ -16,35 +15,6 @@ pub fn convert_ro_to_rid(ro: &ResourceObject, common_path: &Path) -> types::Reso
         .to_str()
         .expect("Directory path must have a valid utf-8 representation");
 
-    // Replace all windows path chars
-    let path_iter = utils::normalize_str_iter(rel_path_str).map(|ch| match ch {
-        '\\' => '/',
-        _ => ch,
-    });
-
-    let res_id_str: String = path_iter.collect();
-    res_id_str.into()
+    rel_path_str.into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::convert_ro_to_rid;
-    use super::ResourceObject::*;
-    use std::path::PathBuf;
-
-    #[test]
-    fn test_convert_unix_path_to_rid() {
-        let common_path = PathBuf::from("");
-        let ro = File("a/b/c/note.md".into());
-        let link = convert_ro_to_rid(&ro, &common_path);
-        assert_eq!(link, "a/b/c/note.md".into())
-    }
-
-    #[test]
-    fn test_convert_windows_path_to_rid() {
-        let common_path = PathBuf::from("");
-        let ro = File("a\\b\\c\\note.md".into());
-        let link = convert_ro_to_rid(&ro, &common_path);
-        assert_eq!(link, "a/b/c/note.md".into())
-    }
-}
