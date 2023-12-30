f54623f9a50b3979de11091c9202541c51fc4a90
Christian Gröling <contact@christiangroeling.de>
2023-12-30 09:33:15 +0100

    refactor: Rename to GetBacklinksAdapter

--- emerald/src/adapters/to_vault/mod.rs
+++ emerald/src/adapters/to_vault/mod.rs
@@ -1,7 +1,7 @@
 mod content_retriever_adapter;
 mod document_metadata_from;
 mod filesystem_metadata_from;
-mod get_backlinks;
+mod get_backlinks_adapter;
 mod get_links;
 mod link_query_result_builder;
 mod note_metadata_retriever;
@@ -9,7 +9,7 @@ mod resource_id_adapters;
 mod resource_id_from;
 
 pub use content_retriever_adapter::ContentRetrieverAdapter;
-pub use get_backlinks::GetBacklinks;
+pub use get_backlinks_adapter::GetBacklinksAdapter;
 pub use get_links::GetLinks;
 pub use note_metadata_retriever::NoteMetadataRetriever;
 pub use resource_id_adapters::convert_resource_ids_to_vault_format;
