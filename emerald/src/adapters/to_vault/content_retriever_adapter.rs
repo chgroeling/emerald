af083c28fcdd72c5f6ca8a233570a3ecc74b0518
Christian Gröling <contact@christiangroeling.de>
2023-12-31 07:39:54 +0100

    doc: Add rustdoc to content_retriever

--- emerald/src/adapters/to_vault/content_retriever_adapter.rs
+++ emerald/src/adapters/to_vault/content_retriever_adapter.rs
@@ -2,12 +2,25 @@ use crate::model::content;
 use crate::model::vault;
 use std::rc::Rc;
 
+/// Adapts a generic markdown content retriever to the `vault::ContentRetriever` interface.
+///
+/// This struct allows the use of any markdown content retriever that implements
+/// `content::MdContentRetriever` to be used wherever a `vault::ContentRetriever` is needed.
 #[derive(Clone)]
 pub struct ContentRetrieverAdapter {
     content_retriever: Rc<dyn content::MdContentRetriever>,
 }
 
 impl ContentRetrieverAdapter {
+    /// Creates a new `ContentRetrieverAdapter`.
+    ///
+    /// # Arguments
+    ///
+    /// * `content_retriever`: An `Rc` pointer to an object implementing `content::MdContentRetriever`.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of `ContentRetrieverAdapter`.
     pub fn new(content_retriever: Rc<dyn content::MdContentRetriever>) -> Self {
         Self { content_retriever }
     }
--- emerald/src/model/vault/content_retriever.rs
+++ emerald/src/model/vault/content_retriever.rs
@@ -1,5 +1,14 @@
 use super::ExResourceId;
 
 pub trait ContentRetriever {
+    /// Retrieves content for the specified resource identifier.
+    ///
+    /// # Arguments
+    ///
+    /// * `rid`: A reference to a `vault::ExResourceId`.
+    ///
+    /// # Returns
+    ///
+    /// A string slice containing the retrieved content.
     fn retrieve(&self, rid: &ExResourceId) -> &str;
 }
